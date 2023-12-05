36dd14b9586d703846840738fb6d0eb68d2e3380
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-12-04 21:46:54 -0400

    chore: add card and input to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./components/button/filled-button";
 export * from "./components/icon/icon";
 export * from "./components/icon-button/icon-button";
+export * from "./components/card/card";
+export * from "./components/input/input";
